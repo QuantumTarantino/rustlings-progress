b0669820b26157894f18fa638c7e586dd0657612
QuantumTarantino <correo@alvaroarias.com>
2022-10-14 00:28:07 +0200

    Strings Done

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,22 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    return input.trim_start().trim_end().to_string();
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut aux: String = input.to_string();
+    aux.push_str(" world!");
+
+    return aux;
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    return input.to_string().replace("cars", "balloons")
 }
 
 #[cfg(test)]
